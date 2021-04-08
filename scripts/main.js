4a4abbb7e9120d05abb96840ae1b17c643da8e43
warshophysical <73259613+warshophysical@users.noreply.github.com>
2021-04-08 16:10:43 -0500

    condition

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,12 @@
 var MyHeading=document.querySelector('h1');
 
-var heading='Hello World'
+var myColor='blue'
+
+if (myColor=='green'){
+    heading='welcome to my green world!';
+} else{
+    heading='welcome to my greenless world'
+    alert('are you at the wrong place?')
+}
+
 MyHeading.textContent=heading;
